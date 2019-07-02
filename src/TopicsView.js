a202f0e656630864ed5bfb7daff19eaed568fe8c
Matt <31932179+MattNoName@users.noreply.github.com>
2019-07-02 18:19:27 -0400

    Added TopicsView

---
+++ src/TopicsView.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class TopicsView extends React.Component{
+
+    constructor(props) {
+        super(props);
+        
+      }
+
+      render() {
+        return(
+  
+          <div>
+          Topics Coming Soon!
+          </div>
+  
+        );
+      }
+
+}
+
+export default TopicsView;
