47338d18c0cc8e9924fa37682298c1f85a4fef19
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2019-07-21 19:37:18 -0400

    caching

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import AppRouter from './AppRouter';
 import teachingStore from './Model/TeachingsStore';
-import * as HomeWorker from './HomeWorker'
+import * as serviceWorker from './serviceWorker'
 
 const store=teachingStore();
  
@@ -21,4 +21,4 @@ const easyMath = (
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-HomeWorker.register();
+serviceWorker.register();
