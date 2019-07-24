1500c0cada0db88a37a8faf5546b257c0fa236cc
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2019-07-24 17:41:09 -0400

    committing deleted or moved files

--- src/HomeConnect.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
- 
-//gives axios connection to the home of the teachings
-const home_connect=axios.create({
-    baseURL: 'http://localhost:5460'
-});
-
-export default home_connect;
