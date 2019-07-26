707c6aad7f8ce74eeeb9289e6ebb8db81d1caacf
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2019-07-26 18:41:44 -0400

    Decided to try different for mobile and desktop/laptop

--- src/View/FractionInput.js
+++ src/View/FractionInput.js
@@ -3,58 +3,66 @@ import './app.css';
 import MathQuill, { addStyles as addMathquillStyles } from 'react-mathquill';
 addMathquillStyles();
 class FractionInput extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      latex: '',
-      text: '',
-      index: this.props.index
+    constructor(props) {
+        super(props);
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+            this.state = {
+            value: '',
+            index: this.props.index
+            }
+        } else {
+            this.state = {
+            latex: '',
+            text: '',
+            index: this.props.index
+            }
+        }
+        this.state.index=this.props.index;
+        window.addEventListener ? window.addEventListener('focus', this.onFoucsChange, true) : window.attachEvent('onfocusout', this.onFoucsChange);
+        window.addEventListener ? window.addEventListener('blur', this.onBlur, true) : window.attachEvent('onblur', this.onBlur);
     }
-    this.state.index=this.props.index;
-    window.addEventListener ? window.addEventListener('focus', this.onFoucsChange, true) : window.attachEvent('onfocusout', this.onFoucsChange);  
-    window.addEventListener ? window.addEventListener('blur', this.onBlur, true) : window.attachEvent('onblur', this.onBlur);
-  }
-  onFoucsChange(){
-    document.activeElement.focus({preventScroll:true});
-    /*
-    var scrollPosition = [
-      document.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
-      document.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
-    ];
-    window.scrollTo(scrollPosition[0], scrollPosition[1]);
-    */
-  }
-  onBlur(){
-
-  }
-  render() {
-    return (
-      <span onKeyDown={(e) => this.onKeyPressed(e)}>
-        <MathQuill className="mathquill-field" onKeyPress={console.log("a")}latex={this.state.latex} onChange={mathField => {
-            const latex = mathField.latex();
-            const text = mathField.text();
-            this.setState({ latex, text });
-            this.props.latexHandler(this.state.index, this.state.latex);
-          }}
-          mathquillDidMount={el => {
-            this.mathQuillEl = el
-          }}/>
-      </span>
-    )
-  }
-  onKeyPressed(e) {
-    switch (e.key) {
-      case 'Tab': case ' ':
-      case '1':case '2':case '3':case '4':case '5':
-      case '6':case '7':case '8':case '9':case '0':
-      case '/':case '-':
-      break;
-      default:
-        e.preventDefault();
+    render() {
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+            return (
+                    <span onKeyDown={(e) => this.onKeyPressed(e)}>
+                    <input value={this.state.value} onChange={(event) => {
+                        this.setState({index: this.state.index, value: event.target.value });
+                        this.props.textHandler(this.state.index, this.state.value);
+                    }}/>
+                    </span>
+                    )
+        } else {
+            return (
+                    <span onKeyDown={(e) => this.onKeyPressed(e)}>
+                    <MathQuill className="mathquill-field" onKeyPress={console.log("a")}latex={this.state.latex} onChange={mathField => {
+                        const latex = mathField.latex();
+                        const text = mathField.text();
+                        this.setState({ latex, text });
+                        this.props.latexHandler(this.state.index, this.state.latex);
+                    }}
+                    mathquillDidMount={el => {
+                    this.mathQuillEl = el
+                    }}/>
+                    </span>
+                    )
+        }
     }
-    if (this.state.latex.match("frac")&&e.key==='/'){
-      e.preventDefault();
+    onKeyPressed(e) {
+        if (e.metaKey && (e.key==="h"||e.key==="q")){
+            return;
+        }
+        switch (e.key) {
+            case 'Tab': case ' ':
+            case '1':case '2':case '3':case '4':case '5':
+            case '6':case '7':case '8':case '9':case '0':
+            case '/':case '-':
+                break;
+            default:
+                e.preventDefault();
+        }
+        if (this.state.latex.match("frac")&&e.key==='/'){
+            e.preventDefault();
+        }
     }
-  }
 }
 export default FractionInput;
