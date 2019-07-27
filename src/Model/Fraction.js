37722f3fe1e09c42f73f133376e8192d7015beb3
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2019-07-27 18:56:11 -0400

    these should have been committed earlier

---
+++ src/Model/Fraction.js
@@ -0,0 +1,13 @@
+class Fraction{
+    numerator
+    denominator
+    fromNumeratorAndDenominator(numerator, denominator){
+        this.numerator=numerator
+        this.denominator=denominator
+    }
+    fromNumerator(numerator){
+        this.numerator=numerator
+        this.denominator=1
+    }
+}
+export default Fraction;
