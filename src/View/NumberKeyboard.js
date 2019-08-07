cde0f7904b1168a02cacdab448270df8abebd273
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2019-08-07 01:51:19 -0400

    NumberKeyboard should have been committed before

---
+++ src/View/NumberKeyboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Keyboard from 'react-simple-keyboard';
+class NumberKeyboard extends React.Component{
+  constructor(props) { super(props); this.state={ keyPressHandler: this.props.keyPressHandler }; };
+render()
+    {
+      return <div className='keyboard-container'>
+        <Keyboard className='Keyboard' layout={{ default: [ " 1 2 3 4 5", " 6 7 8 9 0", " . - / {bksp}", "{space}" ]}}
+          onKeyPress={key => this.state.keyPressHandler(key)} display={{'{bksp}': 'delete', '{space}': 'space', }}/>
+      </div>
+   }
+
+}
+export default NumberKeyboard
