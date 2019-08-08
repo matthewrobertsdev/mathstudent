158e49fe46abc30f75d28e9d1834e7c955697ea0
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2019-08-08 14:20:46 -0700

    forgot to commit TeachingLink

---
+++ src/View/TeachingLink.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './app.css'
+/* for displaying a link to a teaching, that displays its name and loads that teaching when clicked */
+class TeachingLink extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state=this.props.codeName
+        this.state=this.props.singularLowerCase
+      }
+      render() {
+        return(
+          /* will take user to teaching with these names.  Will get teaching from model first. */
+          <Link className={"textLink"} to={`/create/${this.props.codeName}`}>{this.props.displayName}</Link>
+        );
+      }
+}
+export default TeachingLink;
