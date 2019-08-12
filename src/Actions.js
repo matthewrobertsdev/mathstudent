611e9051197a535623b5687e34f04bcf8166e1ec
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2019-08-11 17:12:57 -0700

    Linted Actions

--- src/Actions.js
+++ src/Actions.js
@@ -5,118 +5,56 @@ const _getTeaching = (teaching) => ({ type: 'GET_TEACHING', teaching });
 
 const _createTeaching = (teaching) => ({ type: 'CREATE_TEACHING', teaching });
 
-const _clearTeaching = (teaching) => ({
-    type: 'CLEAR_TEACHING',
-    teaching
-});
-const _updateURL = (URLpathname) => ({
-    type: 'UPDATE_URL',
-    URLpathname
-});
-const _addToInputMap = (keyID, value) => ({
-    type: 'ADD_TO_INPUT_MAP',
-    key: keyID,
-    v: value
-});
-const _updateActiveKeyAndValue = (activeKey) => ({
-    type: 'UPDATE_ACTIVE_KEY_AND_VALUE',
-    activeKey
-});
-const _updateActiveValue = (key) => ({
-    type: 'UPDATE_ACTIVE_VALUE',
-    key
-});
-const _addInputPair = (key, value) => ({
-    type: 'ADD_INPUT_PAIR',
-    key,
-    value
-});
-const _updateActiveKey = (key) => ({
-    type: 'UPDATE_ACTIVE_KEY',
-    key
-});
-const _updateCallingStrings= (callingStrings) => ({
-    type: 'UPDATE_CALLING_STRINGS',
-    callingStrings
-});
-const _updateCreationStrings= (creationStrings) => ({
-    type: 'UPDATE_CREATION_STRINGS',
-    creationStrings
-});
+const _clearTeaching = (teaching) => ({ type: 'CLEAR_TEACHING', teaching });
+
+const _updateURL = (URLpathname) => ({ type: 'UPDATE_URL', URLpathname });
+
+const _addToInputMap = (keyID, value) => ({ type: 'ADD_TO_INPUT_MAP', key: keyID, v: value });
+
+const _updateActiveKeyAndValue = (activeKey) => ({ type: 'UPDATE_ACTIVE_KEY_AND_VALUE', activeKey });
+
+const _updateActiveValue = (key) => ({ type: 'UPDATE_ACTIVE_VALUE', key });
+
+const _addInputPair = (key, value) => ({ type: 'ADD_INPUT_PAIR', key, value });
+
+const _updateActiveKey = (key) => ({ type: 'UPDATE_ACTIVE_KEY', key });
+
+const _updateCallingStrings= (callingStrings) => ({ type: 'UPDATE_CALLING_STRINGS', callingStrings });
+
+const _updateCreationStrings= (creationStrings) => ({ type: 'UPDATE_CREATION_STRINGS', creationStrings });
 
 const _setTeachingObject=(objectName)=>({ type: 'SET_TEACHING_OBJECT', objectName });
 
-//gets all topics for now for teaching
-export const getTopics = () => {
-    return  (dispatch)=>{
-        dispatch(_getTopics(curriculum));
-    };
-};
+/* gets all topics for now for teaching */
+export const getTopics = () => { return  (dispatch)=>{ dispatch(_getTopics(curriculum)); }; };
+
 export const getTeaching = (teachingName) => {
    return (dispatch) => {
     return import(`./Model/math/${teachingName}`).then(teachingObj => {
         dispatch(_getTeaching(teachingObj.default.teaching));
     }).catch(function(error) { console.log(error); }); };
 };
-export const createTeaching = (methodInfo) => {
-    return (dispatch) => {
-        return dispatch(_createTeaching(methodInfo));
-    };
- };
-export const clearTeaching = () => {
-    return (dispatch) => {
-        return dispatch(_clearTeaching({}));
-    };
-};
-export const updateURL = () => {
-    return (dispatch) => {
-        return dispatch(_updateURL(window.location.pathname));
-    };
-};
-export const addToInputMap = (keyID, value) => {
-    return (dispatch) => {
-        return dispatch(_addToInputMap(keyID, value));
-    };
-};
-/*
-export const addToActiveMap = (keyID, value) => {
-    return (dispatch) => {
-        return dispatch(_addToActiveMap(keyID, value));
-    };
-};
-*/
-export const updateActiveKeyAndValue = (keyID, value) => {
-    return (dispatch) => {
-        return dispatch(_updateActiveKeyAndValue(keyID, value));
-    };
-};
-export const updateActiveValue = (key) => {
-    return (dispatch) => {
-        return dispatch(_updateActiveValue(key));
-    };
-};
-export const addInputPair = (key, value) => {
-    return (dispatch) => {
-        return dispatch(_addInputPair(key, value));
-    };
-};
-export const updateActiveKey = (key) => {
-    return (dispatch) => {
-        return dispatch(_updateActiveKey(key));
-    };
-};
-export const updateCallingStrings = (callingStrings) => {
-    return (dispatch) => {
-        return dispatch(_updateCallingStrings(callingStrings));
-    };
-}
-export const updateCreationStrings = (creationStrings) => {
-    return (dispatch) => {
-        return dispatch(_updateCreationStrings(creationStrings));
-    };
-}
-export const setTeachingObject = (objectName) => {
-    return (dispatch) => {
-        return dispatch(_setTeachingObject(objectName));
-    };
-}
+export const createTeaching = (methodInfo) => { return (dispatch) => { return dispatch(_createTeaching(methodInfo)); }; };
+
+export const clearTeaching = () => {return (dispatch) => { return dispatch(_clearTeaching({})); }; };
+
+export const updateURL = () => { return (dispatch) => { return dispatch(_updateURL(window.location.pathname)); }; };
+
+export const addToInputMap = (keyID, value) => { return (dispatch) => { return dispatch(_addToInputMap(keyID, value)); }; };
+
+export const updateActiveKeyAndValue = (keyID, value) => { return (dispatch) => {
+    return dispatch(_updateActiveKeyAndValue(keyID, value)); }; };
+
+export const updateActiveValue = (key) => { return (dispatch) => { return dispatch(_updateActiveValue(key)); }; };
+
+export const addInputPair = (key, value) => { return (dispatch) => { return dispatch(_addInputPair(key, value)); }; };
+
+export const updateActiveKey = (key) => { return (dispatch) => { return dispatch(_updateActiveKey(key)); }; };
+
+export const updateCallingStrings = (callingStrings) => { return (dispatch) => {
+    return dispatch(_updateCallingStrings(callingStrings)); }; }
+
+export const updateCreationStrings = (creationStrings) => { return (dispatch) => { 
+    return dispatch(_updateCreationStrings(creationStrings)); }; }
+
+export const setTeachingObject = (objectName) => { return (dispatch) => { return dispatch(_setTeachingObject(objectName)); }; }
--- src/View/TeachingView.js
+++ src/View/TeachingView.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import {getTeaching, setTeachingObject, clearTeaching} from  '../Actions';
 import 'katex/dist/katex.min.css';
-import { InlineMath, BlockMath } from 'react-katex';
+import { InlineMath } from 'react-katex';
 import './app.css';
 const mapStateToProps = (state) => {
   return {
