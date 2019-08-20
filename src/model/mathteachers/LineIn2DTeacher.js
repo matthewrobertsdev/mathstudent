af7e5246cbdc60f0f3158cdf81abe0aaf4d54f94
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2019-08-20 16:01:50 -0400

    Factored out LineIn2DTeaching from LineIn2DTeacher

--- src/model/mathteachers/LineIn2DTeacher.js
+++ src/model/mathteachers/LineIn2DTeacher.js
@@ -1,24 +1,14 @@
 import MathTeacher from './MathTeacher';
 import AboutLineIn2D from './mathteachings/aboutsections/AboutLineIn2D'
+import LineIn2DTeaching from './mathteachings/LineIn2DTeaching';
 class LineIn2DTeacher extends MathTeacher{
+
+    teaching=LineIn2DTeaching
     constructor(){
         super();
-        this.objectName="LineIn2D";
-        this.displayNameSingular="Line";
-        this.singularLowerCase="line";
-        this.displayNamePlural="Lines";
-        this.pluralLowerCase="lines";
         this.callingStrings=[];
-        this.creationMethodSignatures=[];
-        this.anyNumbers=true;
-        const POINT_SLOPE=["Create a line with a slope and a point", "createPointSlope", "slope", "number", "x1", 
-        "number", "y1", "number"];
-        this.creationMethodSignatures.push(POINT_SLOPE);
-        const POINT_POINT=["Create a line with two points", "createPointPoint", "x1", "number", "y1", "number", 
-        "x2", "number", "y2", "number"];
-        this.creationMethodSignatures.push(POINT_POINT);
         this.about=new AboutLineIn2D().about
-
+        this.creationMethodSignatures=this.teaching.creationMethodSignatures
     }
 }
 export default new LineIn2DTeacher();
