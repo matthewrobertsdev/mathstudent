f701f3e622ad28e1c3118136a53ff5bafc08dc9c
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2019-08-21 20:45:51 -0400

    Work towards limit breach detector

---
+++ src/model/mathteachers/math/LimitBreachDetector.js
@@ -0,0 +1,25 @@
+class LimitBreachDetector{
+
+    /*
+    static isAdditionOutOfLimit(a, b) {
+        var c = a + b;
+        return a !== c-b || b !== c-a;
+      }
+
+     static isMultiplicationOutOfLimit(a, b) {
+        var c = a * b;
+        return a !== c/b || b !== c/a;
+      }
+
+      static isSubtractionOutOfLimit(a, b) {
+        var c = a - b;
+        return a !== c+b || b !== c+a;
+      }
+
+     static isDivisionOutOfLimit(a, b) {
+        var c = a / b;
+        return a !== c*b || 1/b !== a*c;
+      }
+      */
+
+}
