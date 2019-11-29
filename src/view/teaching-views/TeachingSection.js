0ab620b03b5ef4d51ace9a2397a167710a5bf074
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2019-11-29 16:34:07 -0500

    Commenting code and fixed InlineBlock div instead of span

--- src/view/teaching-views/TeachingSection.js
+++ src/view/teaching-views/TeachingSection.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {withRouter} from 'react-router-dom';
-import 'katex/dist/katex.min.css';
 import {BlockMath, InlineMath } from 'react-katex';
+import 'katex/dist/katex.min.css';
 import TeachingLink from './TeachingLink';
 import '../views-general/app.css';
-var Latex = require('react-latex');
+/*
+########################################################
+Renderer for a GUI teaching section based off of a model
+########################################################
+*/
 const mapStateToProps = (state) => { return { teacher: state.teacher, activeMethod: state.activeMethod,
   methodTeacher: state.methodTeacher}; }
 class UnconnectedTeachingSection extends React.Component{
@@ -17,14 +21,18 @@ class UnconnectedTeachingSection extends React.Component{
         );
       }
       displaySection(){
+        //if about section, render that
         if (this.props.purpose==='about'){
             return this.createSection(this.props.teacher.teaching.about)
+            //if method section, render that
         } else if (this.props.purpose==='method'){
             if (this.props.ownMethod===this.props.activeMethod && this.props.methodTeacher && this.props.methodTeacher.concept){
               return this.createSection(this.props.methodTeacher.concept)
+              //if nothing is active, return empty section
             } else {
               return null
             }
+            //if passed a teacher with a concept, render that
         } else if (this.props.teacher.concept){
             return this.createSection(this.props.teacher.concept)
         }
@@ -45,7 +53,7 @@ class UnconnectedTeachingSection extends React.Component{
               } else if (concept.startsWith('{H}')){
                 teaching.push(<h1 key={c} className="main-text-color center-text">{concept.slice(3)}</h1>);
               } else if (concept.startsWith('{IL}')) {
-                teaching.push(<div aria-label="test number"><InlineMath aria-hidden="true" key={c} className='inline-math' >{concept.slice(4)}</InlineMath></div>);
+                teaching.push(<span aria-label="test number"><InlineMath aria-hidden="true" key={c} className='inline-math' >{concept.slice(4)}</InlineMath></span>);
               } else if (concept.startsWith('{BL}')) {
                 teaching.push(<div aria-label="test number"><BlockMath aria-hidden="true" key={c} className='block-math'>{concept.slice(4)}</BlockMath></div>);
               }
