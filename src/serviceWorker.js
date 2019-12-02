469f417324bee7917aaa3df5936e65a698f7d118
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2019-12-02 17:07:07 -0500

    Updated service worker version to 12-2-2019 so that new version caches latest dark mode updates

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -10,7 +10,7 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://bit.ly/CRA-PWA
 
-const date='11-28-2019';
+const date='12-2-2019';
 console.log(date)
 
 const isLocalhost = Boolean(
