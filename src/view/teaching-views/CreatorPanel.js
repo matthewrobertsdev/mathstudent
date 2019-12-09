8c627dc31418e0e9aeb53c6d33da9700bedb34e4
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2019-12-08 19:21:33 -0500

    Refactored so that accordion panel's are composed with ContentPanel

--- src/view/teaching-views/CreatorPanel.js
+++ src/view/teaching-views/CreatorPanel.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import LearnerView from './LearnerView';
 import 'react-simple-keyboard/build/css/index.css';
 import '../views-general/app.css';
-import { AccordionItem, AccordionItemHeading, AccordionItemPanel, AccordionItemButton } from 'react-accessible-accordion';
-import AdView from './AdView';
+import ContentPanel from './ContentPanel';
 import CreatorSection from './CreatorSection';
 
 const mapStateToProps = (state) => {
@@ -15,45 +13,10 @@ const mapStateToProps = (state) => {
 
 class UnconnectedCreatorPanel extends React.Component {
 	
-	render() { return (<div className='fullWidth'> {this.createView()} </div>); }
-	createView() {
-		if (this.props.teacher) {
-			if (this.props.teacher.teaching) {
-				return (
-						<AccordionItem uuid={this.props.uuid}>
-							<AccordionItemHeading>
-								<AccordionItemButton>
-									<span className="center-text main-text-color Heading large-heading-size">
-										Create {this.props.teacher.teaching.displayNamePlural}</span>
-								</AccordionItemButton>
-							</AccordionItemHeading>
-							<AccordionItemPanel>
-								<CreatorSection/>
-							</AccordionItemPanel>
-						</AccordionItem>
-						
-				);
-			}
-		}
+	render() {
+        return <ContentPanel introPhrase="Create " section={<CreatorSection/>}/>
 	}
 
-	createCreatorViews() {
-		if (this.props.teacher.creationMethodSignatures === undefined) { return; }
-		const creatorViews = this.props.teacher.creationMethodSignatures.map((methodSignature, i) => {
-			return (
-				<div key={i} className='fullWidth'>
-					<LearnerView className='CreatorView fullWidth' creator={true} methodSignature={methodSignature}
-						row={this.createCreatorKey(i)} teachingViewRef={this.teachingViewRef}></LearnerView>
-					{this.createAdEverySecond(i)}
-				</div>
-			);
-		});
-		return creatorViews;
-	}
-	createAdEverySecond(i) { if (i - 1 % 2 === 0) { return <div className='fullWidth'><AdView /></div> } }
-
-    createCreatorKey(index) { return 'creator' + this.props.teacher.teaching.objectName + "-" + index; }
-    
 }
 const CreatorPanel = connect(mapStateToProps, null)(UnconnectedCreatorPanel)
 export default CreatorPanel;
--- src/view/teaching-views/LearningPanel.js
+++ src/view/teaching-views/LearningPanel.js
@@ -2,36 +2,21 @@ import React from 'react';
 import { connect } from 'react-redux';
 import 'react-simple-keyboard/build/css/index.css';
 import '../views-general/app.css';
-import {AccordionItem, AccordionItemHeading, AccordionItemPanel, AccordionItemButton } from 'react-accessible-accordion';
-import AdView from './AdView';
 import LearningSection from './LearningSection';
+import ContentPanel from './ContentPanel';
 
 const mapStateToProps = (state) => {
 	return {
-		teacher: state.teacher, displayTeaching: state.displayTeaching,
+		teacher: state.teacher
 	}
 };
 class UnconnectedLearningPanel extends React.Component {
 
 	render() { return (<div className='fullWidth'> {this.createLearningSection()} </div>); }
-	
-	createAdEverySecond(i) { if (i - 1 % 2 === 0) { return <div className='fullWidth'><AdView /></div> } }
-
-	createSpaceEverySecond(i) { if (i - 1 % 2 === 0) { return <div><br></br><br></br></div> } }
 
 	createLearningSection() {
 		if (this.props.teacher.instanceMethodSignatures.length > 0) {
-			return <div className='center-text'><br></br><AccordionItem uuid='working-with'>
-				<AccordionItemHeading>
-					<AccordionItemButton>
-						<span className="main-text-color Heading large-heading-size">
-							Working with {this.props.teacher.teaching.displayNamePlural}</span>
-					</AccordionItemButton>
-				</AccordionItemHeading>
-				<AccordionItemPanel>
-					<LearningSection/>
-				</AccordionItemPanel>
-			</AccordionItem></div>
+		return <ContentPanel introPhrase="Working with " section={<LearningSection/>}/>
 		}
 	}
 
