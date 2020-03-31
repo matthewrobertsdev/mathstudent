05ae8d5f81f678d07c9b629d20a53836f09793cc
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2020-03-31 16:15:24 -0400

    Gold not yellow page not found view now has spacing

--- src/view/views-general/PageNotFoundView.js
+++ src/view/views-general/PageNotFoundView.js
@@ -1,6 +1,6 @@
 import React from 'react';
 const PageNotFoundView = (props) => {
-    return (<span className="center-text main-text-color heading large-heading-size">
-        Sorry, but the page you are looking for cannot be found.</span>)
+    return (<div><br></br><span className="center-text main-text-color heading large-heading-size">
+        Sorry, but the page you are looking for cannot be found.</span></div>)
 }
 export default PageNotFoundView
