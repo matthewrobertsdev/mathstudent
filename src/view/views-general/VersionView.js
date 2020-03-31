a59f9ec71d6d8b360e39bc522b1e2086fcd6eda9
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2020-03-31 09:10:29 -0400

    Version view now a functional component

--- src/view/views-general/VersionView.js
+++ src/view/views-general/VersionView.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import {updateURL} from '../../manager/Actions';
+import {mainHeading} from './Styles'
 const VersionView = (props) => {
     const dispatch=useDispatch()
     useEffect( () => {dispatch(updateURL())})
@@ -10,25 +11,25 @@ const VersionView = (props) => {
     {tellVersion()}
     <br></br><br></br>
     <span 
-    className="center-text main-text-color heading large-heading-size">
+    className={mainHeading}>
         Reload the page to see if a new update has been found.</span>
         <br></br><br></br><span 
-    className="center-text main-text-color heading large-heading-size">
+    className={mainHeading}>
         Latest cached version only runs if you have closed every single page in your browser that has Math Teacher, 
         so that none were open so that the new version could be launched when you next opened a Math Teacher page.</span>
     </div>
     function tellVersion(){
         let lastUpdated=localStorage.getItem('lastUpdated');
         if (lastUpdated){
-            return <span className="center-text main-text-color heading large-heading-size">Latest Cached Version: {lastUpdated}.</span>
+            return <span className={mainHeading}>Latest Cached Version: {lastUpdated}.</span>
         } else {
-            return <span className="center-text main-text-color heading large-heading-size">No version fully cached yet.</span>
+            return <span className={mainHeading}>No version fully cached yet.</span>
         }
     }
     function tellLastChecked(){
         let lastChecked=localStorage.getItem('lastChecked');
         if (lastChecked){
-            return <span className="center-text main-text-color heading large-heading-size">Update found on {lastChecked}.</span>
+            return <span className={mainHeading}>Update found on {lastChecked}.</span>
         }
     }
 }
