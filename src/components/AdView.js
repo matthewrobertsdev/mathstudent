1b8def515c5b7513b01e6206bff17c5874a459e1
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2020-05-17 10:48:37 -0400

    AdView now is one JSX element that changes based on media queries

--- src/components/AdView.js
+++ src/components/AdView.js
@@ -3,23 +3,15 @@ import React from 'react';
 //for large and small-mobile banner for small
 function AdView() {
   return (
-    <div className='center-text'>
-      {/*leaer-board ad for large */}
-      <div className='hide-for-small leader-board-ad center-text'>
+      /* ad for large */
+      <div className='ad center-text'>
         <br></br>
 		    There is an ad here when you are online.
         <br></br>
         Ads help Math Teacher make money.
-      </div>
-       {/*small mobile banner for small */}
-      <div className='hide-for-big mobile-banner center-text'>
         <br></br>
-		    There is an ad here when you are online.
         <br></br>
-        Ads help Math Teacher make money.
-        <br></br><br></br>
       </div>
-    </div>
   );
 }
 export default AdView;
