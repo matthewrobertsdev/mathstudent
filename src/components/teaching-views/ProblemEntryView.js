2ef23d09bc4b9d24d0393a0e0b48e0ee0c58af4f
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2020-05-31 23:09:16 -0400

    Start of making ProblemEntryView inputs controlled components with useState

--- src/components/teaching-views/ProblemEntryView.js
+++ src/components/teaching-views/ProblemEntryView.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { AccordionItem, AccordionItemButton, AccordionItemHeading, AccordionItemPanel } from 'react-accessible-accordion'
+import React, { useState } from 'react';
+import { AccordionItem, AccordionItemButton, AccordionItemHeading, 
+  AccordionItemPanel } from 'react-accessible-accordion'
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-//just provides initial links to enter a problem or generate problems
+//can enter a problem
 const ProblemEntryView = (props) => {
   const teacher = useSelector(state => state.teaching.teacher)
-  let text={}
+  let initialText = {}
   for (let index = 2; index < props.method.length; index++) {
     if (index % 2 == 1) {
-      text[index.toString()]=""
+      initialText[index.toString()] = ""
     }
   }
-  console.log(text)
+  const [text, setText] = useState(initialText);
   if (props.method == null) {
     return null;
   }
-  //else return series of TeachingLinks
   return (
     <span>
       <AccordionItem>
@@ -27,7 +27,10 @@ const ProblemEntryView = (props) => {
         </AccordionItemHeading>
         <AccordionItemPanel>
           {createMethodViews()}
-          <Link to={'./teach/' + teacher.teaching.objectName} className='create-button'>Solve</Link>
+          <Link to={'./teach/' + teacher.teaching.objectName} 
+          className='create-button'>
+            Solve
+          </Link>
         </AccordionItemPanel>
       </AccordionItem>
     </span>
@@ -37,22 +40,23 @@ const ProblemEntryView = (props) => {
     for (let index = 2; index < props.method.length; index++) {
       if (index % 2 == 0) {
         methodViews.push(
-          <label htmlFor={props.method[0]+"-"+props.method[index]} 
-          className='heading medium-line-height small-left-margin label'
-          key={props.method[0]+"-"+props.method[index]+'label'}>
-          {props.method[index]}:
+          <label htmlFor={props.method[0] + "-" + props.method[index]}
+            className='heading medium-line-height small-left-margin label'
+            key={props.method[0] + "-" + props.method[index] + 'label'}>
+            {props.method[index]}:
           </label>
         )
       } else {
         methodViews.push(
-        <input id={props.method[0]+"-"+props.method[index-1]} 
-        className='fixed-small-left-margin'
-        key={props.method[0]+"-"+props.method[index-1]+'input'}/>
+          <input id={props.method[0] + "-" + props.method[index - 1]}
+            className='fixed-small-left-margin'
+            key={props.method[0] + "-" + props.method[index - 1] + 'input'}
+            value={text[index.toString()]}/>
         )
         methodViews.push(
-        <span className='small-space medium-line-height'
-        key={props.method[0]+"-"+props.method[index-1]+'-span'}>
-        </span>
+          <span className='small-space medium-line-height'
+            key={props.method[0] + "-" + props.method[index - 1] + '-span'}>
+          </span>
         )
       }
     }
