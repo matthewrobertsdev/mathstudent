02999408a031ed7d47ef3b6a0995b5f8534bfaf8
matthewrobertsdev <31932179+matthewrobertsdev@users.noreply.github.com>
2021-01-12 23:32:08 -0500

    Start on displaying problems for solving

--- src/pages/SolvePage.js
+++ src/pages/SolvePage.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import UncreatedTeachingView from '../components/teaching-views/UncreatedTeachingView'
 import { getTeacher } from '../store/Actions';
-
+import MathJax from 'react-mathjax'
 
 const SolvePage = (props) => {
   const { match: { params } } = props;
@@ -10,9 +10,17 @@ const SolvePage = (props) => {
   const [teacher, setTeacher] = useState(undefined);
   getTeacher(params.teachingName, setTeacher)
   if (teacher && getMethodIndex()!==-1) {
+    const mathProblem=teacher[params.method+"Problem"](params.parameters.split('@'))
+    const math = mathProblem.split('{$bl}')
     return (
       <main>
         <h1 className='large-left-margin'>{params.teachingName} Problem</h1>
+          <span aria-label={math[2]}>
+            <MathJax.Provider input="tex" >
+              <MathJax.Node aria-hidden='true' className='heading' formula={`\\color{white}{${math[1]}}`} />
+            </MathJax.Provider>
+          </span>
+        )
       </main>
     )
   } else if (teacher === false || getMethodIndex()===-1) {
