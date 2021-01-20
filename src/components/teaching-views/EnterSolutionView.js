bd0fba5a6ef9ec3345fa39e0be2f16a7b3ca6f61
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2021-01-19 22:57:06 -0500

    Inputs are now generated for solutions.

--- src/components/teaching-views/EnterSolutionView.js
+++ src/components/teaching-views/EnterSolutionView.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import MathJax from 'react-mathjax'
 
 // the view where the solving is displayed
 const EnterSolutionView = (props) => {
+  let initialText = {}
+  const [text, setText] = useState(initialText);
   return (
     <div>
-      <h2>Enter Solution in Simplest Form:</h2>
+      <h2>Enter Solution in Simplest Form.</h2>
       {createChoices()}
-      <button className='create-button'>Check Answer</button>
+      <button className='link'>Check Answer</button>
       <br/>
       <br/>
       <br/>
@@ -16,15 +18,49 @@ const EnterSolutionView = (props) => {
   )
 
   function createChoices() {
-    return props.solutions.map(solution => {
+    return props.solutions.solutions.map((solution, index) => {
+      for (let inputIndex = 0; index < inputIndex.length; index++) {
+        text[index.toString()+'-'+inputIndex.toString()] = ""
+      }
+      //getInputs(solution.inputs, index)
       return (
         <div>
           <input type='radio' className='radio-button'/>
           <MathJax.Provider input="tex" >
-              <MathJax.Node inline aria-hidden='true' className='heading' formula={`\\color{white}{${solution.function(solution.inputs)}}`} />
+              <MathJax.Node inline aria-hidden='true' className='heading' 
+              formula={`\\color{white}{${solution.function()}}`} />
             </MathJax.Provider>
-          <br/><br/>
+          <br/>
+          {createInputs(solution.inputs, index)}
+          <br/>
         </div>
+      )
+    })
+  }
+
+  function getInputs(inputs, solutionIndex) {
+    let args=[]
+    for (let index=0; index<inputs.length; index++) {
+      args.push(text[solutionIndex.toString()+'-'+index.toString()])
+    }
+    return args
+  }
+
+  function createInputs(inputs, solutionIndex) {
+    return inputs.map((input, index) => {
+      return (
+      <span><label
+      className='heading medium-line-height label small-right-margin'>
+      {input} is:
+    </label><input className='text-input' value={text[solutionIndex.toString()+'-'+index.toString()]} onChange={
+                  (event) => {
+                    console.log(event.target.value)
+                    setText({
+                      ...text,
+                      [solutionIndex.toString()+'-'+index.toString()]: event.target.value
+                    });
+                  }
+                } /><br/></span>
       )
     })
   }
