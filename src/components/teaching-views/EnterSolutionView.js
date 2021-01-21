b08845f3dc9d0aa57187b9c8fc91adb85efdd46d
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2021-01-21 00:13:47 -0500

    Can now get solutions.  Need to determine if they are correct or not.

--- src/components/teaching-views/EnterSolutionView.js
+++ src/components/teaching-views/EnterSolutionView.js
@@ -1,16 +1,21 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import MathJax from 'react-mathjax'
 import ReactModal from 'react-modal'
 
 // the view where the solving is displayed
 const EnterSolutionView = (props) => {
+  useEffect(()=>{
+    ReactModal.setAppElement('body')
+    setInitialText()
+  })
   let initialText = {}
   let darkBackgroundColor='rgb(34, 34, 33)'
   let lightBackgroundColor='rgb(157, 62, 157)'
   let modalBackgroundColor=lightBackgroundColor
   const [text, setText] = useState(initialText);
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [isCorrect, setIsCorrect] = useState(undefined);
   const [isDarkMode, setIsDarkMode] = useState(window.matchMedia('(prefers-color-scheme: dark)').matches);
+  const [selectedAnswer, setSelectedAnswer]=useState(0)
   window.matchMedia('(prefers-color-scheme: dark)')
       .addEventListener('change', event => {
   if (event.matches) {
@@ -33,33 +38,51 @@ if (isDarkMode) {
       <br/>
       <br/>
       <br/>
-      <ReactModal isOpen={modalIsOpen} style={
+      <ReactModal isOpen={isCorrect!==undefined} style={
     { overlay: {}, content: {width: '200px', height: '200px', margin: 'auto', 
     backgroundColor: modalBackgroundColor, display: 'flex', justifyContent: 'center',
-    alignItems: 'center', flexDirection: 'column'}}}>
-      <h2>Great Job!</h2>
-      <h2>is correct.</h2>
+    alignItems: 'center', flexDirection: 'column', borderRadius: '20px'}}}>
+      <h1>Great Job!</h1>
+      <h2>You are correct.</h2>
       <button className='link' onClick={closeModal}>Got it</button></ReactModal>
     </div>
   )
 
   function handleAnswerSubmit() {
-    setModalIsOpen(true)
+    let answerNum=parseInt(selectedAnswer)
+    let chosenSolution=props.solutions.solutions[answerNum]
+    console.log(chosenSolution.function)
+    let inputTexts=[]
+    for (let index=0; index<chosenSolution.inputs.length; index++) {
+      inputTexts.push(text[selectedAnswer+'-'+index])
+      console.log(inputTexts[index])
+    }
+    setIsCorrect(true)
   }
 
   function closeModal() {
-    setModalIsOpen(false)
+    setIsCorrect(undefined)
   }
+
+  function setInitialText() {
+      for (let index = 0; index < props.solutions.solutions.length; index++)
+        for (let inputIndex = 0; inputIndex < inputIndex.length; inputIndex++) {
+          setText({
+            ...text,[index.toString()+'-'+inputIndex.toString()]: ""
+        })
+      }
+    }
 
   function createChoices() {
     return props.solutions.solutions.map((solution, index) => {
-      for (let inputIndex = 0; index < inputIndex.length; index++) {
-        text[index.toString()+'-'+inputIndex.toString()] = ""
-      }
+      
       //getInputs(solution.inputs, index)
       return (
-        <div>
-          <input type='radio' className='radio-button'/>
+        <div key={index.toString()}>
+          <input type='radio' className='radio-button' value={index} 
+          checked={index===parseInt(selectedAnswer)} onChange={(changeEvent)=>{
+            setSelectedAnswer(changeEvent.target.value)
+          }}/>
           <MathJax.Provider input="tex" >
               <MathJax.Node inline aria-hidden='true' className='heading' 
               formula={`\\color{white}{${solution.function()}}`} />
@@ -85,12 +108,12 @@ if (isDarkMode) {
   function createInputs(inputs, solutionIndex) {
     return inputs.map((input, index) => {
       return (
-      <span><label
+      <span key={solutionIndex.toString()+'-'+index.toString()}><label
       className='heading medium-line-height label small-right-margin'>
       {input} is:
-    </label><input className='text-input' value={text[solutionIndex.toString()+'-'+index.toString()]} onChange={
+    </label><input className='text-input' value={text[solutionIndex.toString()+'-'+index.toString()]} 
+     onChange={
                   (event) => {
-                    console.log(event.target.value)
                     setText({
                       ...text,
                       [solutionIndex.toString()+'-'+index.toString()]: event.target.value
